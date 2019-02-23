2dcedd508efe2b2e8ed802d2f2b6f44de7967b03
Jake Crawford <jake.crawford@outlook.com>
2019-02-23 14:17:14 -0700

    Add api post code

--- app/BikeAlong/GeoLocation.js
+++ app/BikeAlong/GeoLocation.js
@@ -20,6 +20,25 @@ class Geolocation extends Component {
           longitude: position.coords.longitude,
           error: null,
         });
+        fetch('https://SERVERADDRESS/route/status', {
+          method: 'POST',
+          headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            latitude: this.state.latitude,
+            longitude: this.state.longitude,
+          }),
+        })
+        .then(response => {
+          console.log("Response:")
+          console.log(response);
+        })
+        .catch(error => {
+          console.log("Error:")
+          console.log(error);
+        });
       },
       (error) => this.setState({ error: error.message }),
       { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 },
@@ -39,7 +58,6 @@ class Geolocation extends Component {
       <View style={{ flexGrow: 1, alignItems: 'center', justifyContent: 'center' }}>
         <Text>Latitude: {this.state.latitude}</Text>
         <Text>Longitude: {this.state.longitude}</Text>
-        {this.state.error ? <Text>Error: {this.state.error}</Text> : null}
       </View>
     );
   }
