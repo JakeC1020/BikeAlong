11dd4f981b30ba480c197955a1b266bad107d1eb
Ryan Rouleau <ryan@ryanrouleau.com>
2019-02-23 15:04:08 -0700

    wip

--- frontend/bike-along/src/App.js
+++ frontend/bike-along/src/App.js
@@ -6,7 +6,7 @@ import Map from './components/Map.js';
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="App" style={{height: '100%'}}>
         <Map />
       </div>
     );
--- frontend/bike-along/src/components/Map.js
+++ frontend/bike-along/src/components/Map.js
@@ -4,8 +4,13 @@ import {
   withScriptjs, 
   withGoogleMap, 
   GoogleMap, 
-  Marker 
+  Marker,
+  BicyclingLayer,
 } from 'react-google-maps';
+
+import { googleMapsKey } from '../secrets.js';
+
+const googleMapsUrl = `https://maps.googleapis.com/maps/api/js?v=3.exp&key=${googleMapsKey}&libraries=geometry,drawing`;
 
 const boulderCoords = {
   lat: 40.010193,
@@ -18,52 +23,145 @@ const Loading = () => (
   </>
 );
 
-//class Map extends React.Component {
-  //render() {
-    //return (
-      //<GoogleMap
-        //defaultZoom={8}
-        //defaultCenter={boulderCoords}
-      //>
-        //<Marker position={boulderCoords} />
-      //</GoogleMap>
-    //)
-  //}
-//};
+const mapStyle = {
+  width: '100%',
+  height: '100%',
+  cursor: 'none !important',
+}
 
-//const Map = compose(
-  //withProps({
-    //googleMapURL: "https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places",
-    //loadingElement: Loading,
-  //}),
-  //withScriptjs,
-  //withGoogleMap
-//)(props => (
-  //<GoogleMap
-    //defaultZoom={8}
-    //defaultCenter={boulderCoords}
-  //>
-    //<Marker position={boulderCoords} />
-  //</GoogleMap>
+const defaultOptions = {
+  draggableCursor: 'default',
+  disableDefaultUI: true,
+  gestureHandling: 'none',
+  zoomControl: false,
+  styles: [
+    {
+      "featureType": "poi",
+      "elementType": "labels.icon",
+      "stylers": [
+        {
+          "visibility": "off"
+        }
+      ]
+    },
+    {
+      featureType: "transit",
+      elementType: "all",
+      stylers: [
+        { visibility: "off" }
+      ]
+    },
+    {elementType: 'geometry', stylers: [{color: '#242f3e'}]},
+    {elementType: 'labels.text.stroke', stylers: [{color: '#242f3e'}]},
+    {elementType: 'labels.text.fill', stylers: [{color: '#746855'}]},
+    {
+      featureType: 'administrative.locality',
+      elementType: 'labels.text.fill',
+      stylers: [{color: '#d59563'}]
+    },
+    {
+      featureType: 'poi',
+      elementType: 'labels.text.fill',
+      stylers: [{color: '#d59563'}]
+    },
+    {
+      featureType: 'poi.park',
+      elementType: 'geometry',
+      stylers: [{color: '#263c3f'}]
+    },
+    {
+      featureType: 'poi.park',
+      elementType: 'labels.text.fill',
+      stylers: [{color: '#6b9a76'}]
+    },
+    {
+      featureType: 'road',
+      elementType: 'geometry',
+      stylers: [{color: '#38414e'}]
+    },
+    {
+      featureType: 'road',
+      elementType: 'geometry.stroke',
+      stylers: [{color: '#212a37'}]
+    },
+    {
+      featureType: 'road',
+      elementType: 'labels.text.fill',
+      stylers: [{color: '#9ca5b3'}]
+    },
+    {
+      featureType: 'road.highway',
+      elementType: 'geometry',
+      stylers: [{color: '#746855'}]
+    },
+    {
+      featureType: 'road.highway',
+      elementType: 'geometry.stroke',
+      stylers: [{color: '#1f2835'}]
+    },
+    {
+      featureType: 'road.highway',
+      elementType: 'labels.text.fill',
+      stylers: [{color: '#f3d19c'}]
+    },
+    {
+      featureType: 'transit',
+      elementType: 'geometry',
+      stylers: [{color: '#2f3948'}]
+    },
+    {
+      featureType: 'transit.station',
+      elementType: 'labels.text.fill',
+      stylers: [{color: '#d59563'}]
+    },
+    {
+      featureType: 'water',
+      elementType: 'geometry',
+      stylers: [{color: '#17263c'}]
+    },
+    {
+      featureType: 'water',
+      elementType: 'labels.text.fill',
+      stylers: [{color: '#515c6d'}]
+    },
+    {
+      featureType: 'water',
+      elementType: 'labels.text.stroke',
+      stylers: [{color: '#17263c'}]
+    }
+  ]
+}
 
-//));
+class Map extends React.Component {
 
-const Map = compose(
+  mapClick(e) {
+    console.log(e);
+  }
+
+  render() {
+    return (
+      <GoogleMap
+        defaultCenter={boulderCoords}
+        defaultOptions={defaultOptions}
+        defaultZoom={18}
+        onClick={e => this.mapClick(e)}
+        style={{width: '100%', height: '100%'}}
+      >
+        <Marker position={boulderCoords} />
+      </GoogleMap>
+    )
+  }
+}
+
+const ComposedMap = compose(
   withProps({
-    googleMapURL: "https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places",
+    googleMapURL: googleMapsUrl,
     loadingElement: <div style={{ height: `100%` }} />,
-    containerElement: <div style={{ height: `400px` }} />,
+    containerElement: <div style={{ height: `100%` }} />,
     mapElement: <div style={{ height: `100%` }} />,
   }),
   withScriptjs,
   withGoogleMap
-)((props) =>
-  <GoogleMap
-    defaultZoom={8}
-    defaultCenter={{ lat: -34.397, lng: 150.644 }}
-  >
-    {props.isMarkerShown && <Marker position={{ lat: -34.397, lng: 150.644 }} />}
-  </GoogleMap>
-);
+)(Map);
 
-export default Map;
+export default ComposedMap;
